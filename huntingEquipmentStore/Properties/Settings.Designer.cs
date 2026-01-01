c0f020b3ebba15b4b623028bf4ade05f2d8f0c32
Tomsa Luca <lucasebastian777@gmail.com>
2026-01-01 20:25:29 +0200

    cleanup

--- huntingEquipmentStore/Properties/Settings.Designer.cs
+++ huntingEquipmentStore/Properties/Settings.Designer.cs
@@ -26,24 +26,11 @@ namespace huntingEquipmentStore.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\.richbank\\c#\\huntingEquipmentStore\\h" +
-            "untingEquipmentStore\\hunting_equipment_store.mdf;Integrated Security=True;Connec" +
-            "t Timeout=30;User Instance=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=|DataDirectory|\\hunting_equipment_store" +
+            ".mdf;Integrated Security=True;Connect Timeout=30;User Instance=True")]
         public string hunting_equipment_storeConnectionString {
             get {
                 return ((string)(this["hunting_equipment_storeConnectionString"]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;AttachDbFilename=C:\\.richbank\\c#\\huntingEquipmentStore\\h" +
-            "untingEquipmentStore\\hunting_equipment_store.mdf;Integrated Security=True;Connec" +
-            "t Timeout=30;User Instance=True")]
-        public string hunting_equipment_storeConnectionString1 {
-            get {
-                return ((string)(this["hunting_equipment_storeConnectionString1"]));
             }
         }
     }
